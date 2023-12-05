cb3d6cc6d45eab91dde1d134274415a37b72203d
emmanuel-kipngeno <immanuelkipngeno7387@gmail.com>
2023-12-05 23:04:49 +0300

    completed assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,40 @@
 def write_file(file_name, file_content):
-    pass
+    """
+    Write content to a .txt file.
+
+    :param file_name: The name of the file without extension.
+    :type file_name: str
+    :param file_content: The content to be written to the file.
+    :type file_content: str
+    """
+    file_name_with_extension = f"{file_name}.txt"
+    with open(file_name_with_extension, 'w') as file:
+        file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    """
+    Append content to a .txt file.
+
+    :param file_name: The name of the file without extension.
+    :type file_name: str
+    :param append_content: The content to be appended to the file.
+    :type append_content: str
+    """
+    file_name_with_extension = f"{file_name}.txt"
+    with open(file_name_with_extension, 'a') as file:
+        file.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    """
+    Read content from a .txt file.
+
+    :param file_name: The name of the file without extension.
+    :type file_name: str
+    :return: The content of the file.
+    :rtype: str
+    """
+    file_name_with_extension = f"{file_name}.txt"
+    with open(file_name_with_extension, 'r') as file:
+        return file.read()
